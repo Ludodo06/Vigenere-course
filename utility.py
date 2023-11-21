6961f303564d859530f22e73e9ea8e70a87377c6
Ludo <pujolludo@gmail.com>
2023-11-21 11:49:55 +0100

    plt black

--- utility.py
+++ utility.py
@@ -39,7 +39,7 @@ def plot_letters_frequency_analysis(input_string):
     # print(freq_list)
     alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     plt.figure(figsize=(10,3))
-    plt.bar(alphabet, freq_list)
+    plt.bar(alphabet, freq_list, color="black")
     plt.tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
     for spine in plt.gca().spines.values():
         spine.set_visible(False)
